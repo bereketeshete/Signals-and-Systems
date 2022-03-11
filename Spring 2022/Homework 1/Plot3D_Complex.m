63f0b31e616435a3853ccaf94a09793e760484b0
Bereket Eshete <40177156+bereketeshete@users.noreply.github.com>
2022-03-11 15:59:26 -0600

    Update Plot3D_Complex.m

--- Spring 2022/Homework 1/Plot3D_Complex.m
+++ Spring 2022/Homework 1/Plot3D_Complex.m
@@ -1,7 +1,7 @@
 a = 4:0.1:5; % real axis
 b = -1:0.1:1; % imaginary axis
 
-[A,B] = meshgrid(a,b); % this is the first command used to plot any 3D surfaces to form matrix
+[A,B] = meshgrid(a,b); 
 
 x = A + 1i*B;
 y = (x-2).*(x-7) + 7; 
